3c1b2bf946c569e84da19edc2740997400518ccd
Sergey Nikolenko <snikolenko@gmail.com>
2014-06-27 00:25:38 +0400

    finding maximal elements

--- src/ims-bicluster.cpp
+++ src/ims-bicluster.cpp
@@ -50,10 +50,6 @@ double max_force = 120;
 // distance inside which we introduce spatial edges
 double threshold = 1.5;
 
-// k = round(8*spectors/9);
-// r = 0.20;
-
-
 int help(char *argv[]) {
     cout << "Usage: " << argv[0] << " [--mat] --input=input_file" << endl;
     return 0;
@@ -89,9 +85,9 @@ int main(int argc, char *argv[]) {
     matvar_t *matvar;
 
     uint num_pixels, len_spectrum;
-    gsl_matrix *g_spectra;
+    double **spectra;
     double *maxima, *specsdiag, *pixdiag;
-
+    
     if (matlab_input) {
         LOG("Reading input from " << input_filename << " as a Matlab file...");
         matfp = Mat_Open(input_filename.c_str(), MAT_ACC_RDONLY);
@@ -105,7 +101,7 @@ int main(int argc, char *argv[]) {
         num_pixels = matvar->dims[0];
         len_spectrum = matvar->dims[1];
 
-        g_spectra = gsl_matrix_alloc(num_pixels, len_spectrum);
+        spectra = allocate_2d_with_default<double>(num_pixels, len_spectrum, 0);
         maxima = allocate_1d_with_default<double>(num_pixels, 0);
         specsdiag = allocate_1d_with_default<double>(len_spectrum, 0);
         pixdiag = allocate_1d_with_default<double>(num_pixels, 0);
@@ -115,7 +111,7 @@ int main(int argc, char *argv[]) {
         for (uint i=0; i<num_pixels; ++i) {
             for (uint j=0; j<len_spectrum; ++j) {
                 val = ((double*)(matvar->data))[num_pixels*j+i];
-                gsl_matrix_set(g_spectra, i, j, val);
+                spectra[i][j] = val;
                 specsdiag[j] += val;
                 pixdiag[i] += val;
                 if (val > maxima[i]) maxima[i] = val;
@@ -128,37 +124,59 @@ int main(int argc, char *argv[]) {
     uint k = (uint)(8*num_pixels/(double)9);
     double r = 0.20;
 
-
-
-
-
-    double data[] = { 1.0  , 1/2.0, 1/3.0, 1/4.0,
-                    1/2.0, 1/3.0, 1/4.0, 1/5.0,
-                    1/3.0, 1/4.0, 1/5.0, 1/6.0,
-                    1/4.0, 1/5.0, 1/6.0, 1/7.0 };
-
-    gsl_matrix_view m = gsl_matrix_view_array (data, 4, 4);
-
-    gsl_vector *eval = gsl_vector_alloc (4);
-    gsl_matrix *evec = gsl_matrix_alloc (4, 4);
-
-    gsl_eigen_symmv_workspace * w =  gsl_eigen_symmv_alloc (4);
-    gsl_eigen_symmv (&m.matrix, eval, evec, w);
-    gsl_eigen_symmv_free (w);
-    gsl_eigen_symmv_sort (eval, evec, GSL_EIGEN_SORT_ABS_ASC);
-
-    LOG("Sample output from gsl_eigen:")  
-    for (uint i = 0; i < 4; ++i) {
-        double eval_i = gsl_vector_get (eval, i);
-        gsl_vector_view evec_i = gsl_matrix_column (evec, i);
-
-        LOG ("\teigenvalue = " << eval_i);
-        LOG ("\teigenvector = ");
-        gsl_vector_fprintf (stdout, &evec_i.vector, "%g");
+    // find maximal elements
+    LOG("Finding maximal elements...");
+    uint **U = allocate_2d_with_default<uint>(num_pixels, len_spectrum, 0);
+    vector<uint> indices(len_spectrum);
+    for (uint j=0; j<len_spectrum; ++j) { indices.push_back(j); }
+    for (uint i=0; i<num_pixels; ++i) {
+        sort(begin(indices), end(indices), [&](const uint & j1, const uint & j2) {
+            return spectra[i][j1] > spectra[i][j2];
+        });
+        for (uint j=0; j<k; ++j) {
+            U[i][indices[j]] = 1;
+        }
+        // LOG("\t" << spectra[i][indices[0]] << "\t" << spectra[i][indices[1]] << "\t" << spectra[i][indices[2]]);
     }
 
-    gsl_matrix_free(g_spectra);
+    LOG("Filling incidence matrix...");
+    double **W = allocate_2d_with_default<double>(num_pixels, num_pixels, 0);
+    double *on_diag = allocate_1d_with_default<double>(num_pixels, 0);
+    double *little = allocate_1d_with_default<double>(num_pixels, 0);
+    
+
+    delete maxima;
+    delete specsdiag;
+    delete pixdiag;
+    delete_2d<double>(num_pixels, spectra);
 
     LOG("All done.");
     return 0;
 }
+
+
+    // double data[] = { 1.0  , 1/2.0, 1/3.0, 1/4.0,
+    //                 1/2.0, 1/3.0, 1/4.0, 1/5.0,
+    //                 1/3.0, 1/4.0, 1/5.0, 1/6.0,
+    //                 1/4.0, 1/5.0, 1/6.0, 1/7.0 };
+
+    // gsl_matrix_view m = gsl_matrix_view_array (data, 4, 4);
+
+    // gsl_vector *eval = gsl_vector_alloc (4);
+    // gsl_matrix *evec = gsl_matrix_alloc (4, 4);
+
+    // gsl_eigen_symmv_workspace * w =  gsl_eigen_symmv_alloc (4);
+    // gsl_eigen_symmv (&m.matrix, eval, evec, w);
+    // gsl_eigen_symmv_free (w);
+    // gsl_eigen_symmv_sort (eval, evec, GSL_EIGEN_SORT_ABS_ASC);
+
+    // LOG("Sample output from gsl_eigen:")  
+    // for (uint i = 0; i < 4; ++i) {
+    //     double eval_i = gsl_vector_get (eval, i);
+    //     gsl_vector_view evec_i = gsl_matrix_column (evec, i);
+
+    //     LOG ("\teigenvalue = " << eval_i);
+    //     LOG ("\teigenvector = ");
+    //     gsl_vector_fprintf (stdout, &evec_i.vector, "%g");
+    // }
+
