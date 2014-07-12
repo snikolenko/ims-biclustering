f565e44f0391b39193252a894c1b026553314fbc
Sergey Nikolenko <snikolenko@gmail.com>
2014-07-12 17:29:42 +0400

    minor

--- gen-all.sh
+++ gen-all.sh
@@ -3,9 +3,8 @@
 d=$1
 echo "Running ims-bicluster for $d..."
 ./bin/ims-bicluster --mat --input=data/$d --eigens=20
-gen-report $d
 suffix=$(date '+%y%m%d-%H%M%S')
 echo "Making report for $d..."
-reportname=$(gen-report $d)
-echo "Report written to reports/$reportname"
+reportname=$(bash gen-report.sh $d)
+echo "Report written to reports/$reportname.pdf"
 exit
--- gen-report.sh
+++ gen-report.sh
@@ -2,8 +2,7 @@
 
 d=$1
 suffix=$(date '+%y%m%d-%H%M%S')
-echo "Making report for $d..."
 python python/visualize.py -e data/$d.val.csv -v data/$d.vec.csv -c data/$d.coords.csv -N 10 -M 10 -t $d
 cd reports/latex && pdflatex tmp >/dev/null && pdflatex tmp >/dev/null && mv tmp.pdf ../$d-$suffix.pdf && cd ../..
-echo "Done!"
+echo $d-$suffix
 exit
