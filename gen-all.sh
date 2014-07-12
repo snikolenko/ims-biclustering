67040857721d71e7ea181dfc312c99ac5a006349
Sergey Nikolenko <snikolenko@gmail.com>
2014-07-12 17:37:01 +0400

    mode

--- gen-all.sh
+++ gen-all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 d=$1
+mode=$2
 echo "Running ims-bicluster for $d..."
-./bin/ims-bicluster --mat --input=data/$d --eigens=20
+./bin/ims-bicluster --mat$mode --input=data/$d --eigens=20
 suffix=$(date '+%y%m%d-%H%M%S')
 echo "Making report for $d..."
 reportname=$(bash gen-report.sh $d)
